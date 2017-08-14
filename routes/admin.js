ec616febb1fb1d103c10d8bc57501e8f53a74247
liyq <yongqiang.li@futengwl.com>
2017-08-14 11:32:36 +0800

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var checkToekn = require('../middleware/checkToken');
 var router = express.Router();
-router.get('/admin',checkToekn,function (req,res,next){
+router.get('/admin', checkToekn, function (req, res, next) {
   res.send({
-    type:true,
-    name:'dailu'
+    type: true,
+    name: 'dailu'
   });
 });
 module.exports = router
